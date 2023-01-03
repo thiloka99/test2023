a951fb025b9e59560a966f16fe391bf319dbce98
thiloka99 <107987232+thiloka99@users.noreply.github.com>
2023-01-03 14:15:02 +0530

    Update sum.java

--- sum.java
+++ sum.java
@@ -2,7 +2,8 @@ class sum{
     public static void main(String args[]){
         int a = 3;
         int b = 8;//b value
-        int c = 4;
+        int c = 4; 
+        int d=9;
         System.out.println(a+b);
     }
 }
